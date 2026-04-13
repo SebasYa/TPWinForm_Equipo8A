17a66df8d715d796e8d210fe2e236af4571a7b84
Marcelo Rearte Becerra <74743470+marceloreart@users.noreply.github.com>
2026-04-13 20:40:42 -0300

    Creacion de la clase Articulo

---
+++ TPWinForm_equipo-8A/Articulo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_8A
+{
+    internal class Articulo
+    {
+    }
+}
