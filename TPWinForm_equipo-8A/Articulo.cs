697da5b2002133cd920ccb74489d6d7b798cfd41
Marcelo Rearte Becerra <74743470+marceloreart@users.noreply.github.com>
2026-04-13 21:08:56 -0300

    Clase Articulo - se agregaron props & constructor

--- TPWinForm_equipo-8A/Articulo.cs
+++ TPWinForm_equipo-8A/Articulo.cs
@@ -8,5 +8,24 @@ namespace TPWinForm_equipo_8A
 {
     internal class Articulo
     {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca IdMarca { get; set; }
+        //public Categoria IdCategoria { get; set; }
+        public decimal Precio { get; set; }
+
+        /*
+        public Articulo(string codigo, string nombre, string descripcion, Marca idMarca, Categoria idCategoria, decimal precio)
+        {
+            Codigo = codigo;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            IdMarca = idMarca;
+            IdCategoria = idCategoria;
+            Precio = precio;
+        }
+        */
     }
 }
+    
