b3fdc2506ca7e80681f22827b1ac91a77bd46af3
Marcelo Rearte Becerra <74743470+marceloreart@users.noreply.github.com>
2026-04-16 20:00:38 -0300

    Se creó la arquitectura en capas.

--- dominio/Articulo.cs
+++ dominio/Articulo.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TPWinForm_equipo_8A
+namespace dominio
 {
-    internal class Articulo
+    public class Articulo
     {
         public string Codigo { get; set; }
         public string Nombre { get; set; }
