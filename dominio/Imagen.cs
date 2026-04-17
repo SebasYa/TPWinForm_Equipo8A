ab69e3e26d9df52bef5dff142989e804fdd87af0
Sebastian Yanni <160461431+SebasYa@users.noreply.github.com>
2026-04-16 21:29:09 -0300

    Se cambia a public la clase Imagen

--- dominio/Imagen.cs
+++ dominio/Imagen.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    internal class Imagen
+    public class Imagen
     {
         public int Idd { get; set; }
         public Articulo IdArticulo { get; set; }
