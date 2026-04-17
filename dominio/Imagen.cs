d969fa5d1070508ac82473b1f22a22edaa19a708
Sebastian Yanni <160461431+SebasYa@users.noreply.github.com>
2026-04-16 21:21:38 -0300

    Se crea clase Imagen

---
+++ dominio/Imagen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    internal class Imagen
+    {
+        public int Idd { get; set; }
+        public Articulo IdArticulo { get; set; }
+        public string ImagenUrl { get; set; }
+    }
+}
